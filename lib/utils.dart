bfbefd0b6d651a4cdf6ee691c266846677394777
TcBello <78838480+TcBello@users.noreply.github.com>
2021-04-09 17:01:29 +0800

    change miniplayer

---
+++ lib/utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+double valueFromPercentageInRange(
+    {@required final double min, max, percentage}) {
+  return percentage * (max - min) + min;
+}
+
+double percentageFromValueInRange({@required final double min, max, value}) {
+  return (value - min) / (max - min);
+}
